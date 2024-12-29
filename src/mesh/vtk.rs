afae944943f95414e9eed00ffed9ddaf7e349016
echolmy <echoliumy@outlook.com>
2024-12-29 21:36:04 +0000

    modify vtk import for legacy format, unregistedgrid and triangle cell

--- src/mesh/vtk.rs
+++ src/mesh/vtk.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use bevy::render::mesh::{Indices, PrimitiveTopology, VertexAttributeValues};
 use bevy::render::render_asset::RenderAssetUsages;
 use std::path::PathBuf;
-use vtkio::model::{CellType, Cells, VertexNumbers};
+use vtkio::model::{CellType, Cells, Piece, UnstructuredGridPiece, VertexNumbers};
 use vtkio::*;
 
 pub fn load_vtk(path: &PathBuf) -> Vtk {
@@ -13,7 +13,7 @@ pub fn load_vtk(path: &PathBuf) -> Vtk {
         panic!("Failed to load VTK file: {}", vtk_path.display());
     }
 }
-pub fn process_vtk_mesh(vtk: &Vtk) -> Option<Mesh> {
+pub fn process_vtk_mesh_legacy(vtk: &Vtk) -> Option<Mesh> {
     let mut mesh = Mesh::new(
         PrimitiveTopology::TriangleList,
         RenderAssetUsages::default(),
@@ -22,112 +22,74 @@ pub fn process_vtk_mesh(vtk: &Vtk) -> Option<Mesh> {
     match &vtk.data {
         // 1. UnstructuredGrid
         model::DataSet::UnstructuredGrid { meta: _, pieces } => {
-            let mut vertices_all_pieces: Vec<Vec<[f32; 3]>> = Vec::new(); //暂时不用，视情况看以后有了多pieces的数据再看
-            let mut vertices: Vec<[f32; 3]> = Vec::new();
-            let mut indices_all_pieces: Vec<Vec<u32>> = Vec::new(); //暂时不用，视情况看以后有了多pieces的数据再看
-            let mut indices: Vec<u32> = Vec::new();
-            for piece in pieces {
-                match piece {
-                    // 1.1 process vertices data
-                    // Piece::Inline
-                    model::Piece::Inline(grid_piece) => {
-                        let points = grid_piece
-                            .points
-                            .cast_into::<f32>()
-                            .expect("IOBuffer converted failed.");
-
-                        let vertices_per_piece: Vec<[f32; 3]> =
-                            points.chunks_exact(3).map(|p| [p[0], p[1], p[2]]).collect();
-
-                        vertices_all_pieces.push(vertices_per_piece.clone());
-                        vertices.append(&mut vertices_per_piece.clone());
-
-                        // 1.2 process vertices indices
-                        let mut indices_per_piece = Vec::new();
-                        process_vtk_cells(
-                            vertices_per_piece,
-                            &mut indices_per_piece,
-                            &grid_piece.cells,
-                        );
-
-                        indices.append(&mut indices_per_piece);
-                        indices_all_pieces.push(indices_per_piece);
-                    }
-
-                    // Piece::{Source, Loaded}
-                    _ => {
-                        println!("Source or Loaded");
-                    }
-                }
-            }
-
-            // 1.2 create mesh
-            println!("Finished processing mesh. mesh vertices: {:?}", vertices);
-            mesh.insert_attribute(
-                Mesh::ATTRIBUTE_POSITION,
-                VertexAttributeValues::from(vertices.clone()),
-            );
-
-            println!("indices:{:?}", indices);
-            mesh.insert_indices(Indices::U32(indices.clone()));
-
-            mesh.compute_normals();
-
+            _process_legacy_unstructured_grid(&mut mesh, pieces);
             Some(mesh)
         }
-        // enum ImageData, StructuredGrid, RectilinearGrid, PolyData, Field
-        _ => None,
+        _ => {
+            println!("Unsupported Other DataSet. Please select UnstructuredGrid");
+            None
+        }
     }
 }
 
-fn process_vtk_cells(
-    // TODO: Unused variable
-    _vertices_per_piece: Vec<[f32; 3]>,
-    indices_per_piece: &mut Vec<u32>,
-    cells: &Cells,
-) {
-    match &cells.cell_verts {
-        VertexNumbers::Legacy {
-            num_cells: _,
-            vertices,
-        } => {
-            // read data from vertices
-            // e.g. vertices = [4, 0, 1, 2, 3]:
-            // 4 : number of vertices
-            // [0,1,2,3] : index
-            let mut current_index = 0;
-            for cell_type in &cells.types {
-                let n_vertices = vertices[current_index] as usize;
-                let vertex_indices =
-                    vertices[current_index + 1..current_index + 1 + n_vertices].to_vec();
-                match cell_type {
-                    CellType::Tetra => {
-                        indices_per_piece.extend_from_slice(&[
-                            vertex_indices[0],
-                            vertex_indices[2],
-                            vertex_indices[1], // face 1
-                            vertex_indices[0],
-                            vertex_indices[3],
-                            vertex_indices[2], // face 2
-                            vertex_indices[0],
-                            vertex_indices[1],
-                            vertex_indices[3], // face 3
-                            vertex_indices[1],
-                            vertex_indices[2],
-                            vertex_indices[3], // face 4
-                        ]);
-                    }
-                    _ => println!("TODO: Unsupported cell type: {:?}", cell_type),
-                }
-                current_index += 1 + n_vertices;
-            }
-            // println!("{:?}", num_cells);
-        }
-        VertexNumbers::XML {
-            connectivity,
-            offsets,
-        } => {
-            println!("{:?}, {:?}", connectivity, offsets);
+fn _process_legacy_unstructured_grid(mesh: &mut Mesh, pieces: &[Piece<UnstructuredGridPiece>]) {
+    let mut vertices: Vec<[f32; 3]> = Vec::new();
+    let indices: Vec<u32>;
+    // legacy should be only one piece::inline
+
+    // 1. process geometry data
+    if let Piece::Inline(piece_ptr) = pieces
+        .get(0)
+        .expect("[Legacy format] Pieces array is empty. Please check")
+    {
+        // 1.1 process point position
+        let points = piece_ptr
+            .points
+            .cast_into::<f32>()
+            .expect("IOBuffer converted failed.");
+        vertices.reserve(piece_ptr.num_points());
+        vertices = points.chunks_exact(3).map(|p| [p[0], p[1], p[2]]).collect();
+
+        // 1.2. process point indices to construct triangle
+        indices = _process_legacy_unstructuredgrid_cells_triangle(piece_ptr.clone());
+    } else {
+        panic!("First piece must be an Inline piece")
+    }
+
+    // 2. insert data to mesh
+    // 2.1. insert points positions
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        VertexAttributeValues::from(vertices),
+    );
+    // 2.2. insert points indices
+    mesh.insert_indices(Indices::U32(indices));
+
+    // 2.3 compute normals
+    mesh.compute_normals();
+}
+
+fn _process_legacy_unstructuredgrid_cells_triangle(
+    piece_ptr: Box<UnstructuredGridPiece>,
+) -> Vec<u32> {
+    // check every cell type
+    for _type in &piece_ptr.cells.types {
+        if *_type != CellType::Triangle {
+            panic!("cell type contains non-triangle");
         }
     }
+    let mut indices: Vec<u32> = Vec::with_capacity(piece_ptr.cells.num_cells() * 3);
+    let cell_data = piece_ptr.cells.cell_verts.into_legacy();
+
+    if cell_data.1.len() % 4 != 0 {
+        panic!("number of [CELLS] are not multiply of 4.")
+    }
+
+    for chunk in cell_data.1.chunks_exact(4) {
+        indices.push(chunk[1]);
+        indices.push(chunk[2]);
+        indices.push(chunk[3]);
+    }
+
+    indices
 }
