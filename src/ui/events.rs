0165975a13c0bc4be7bb5e2bc686c3bdec21929d
echolmy <echoliumy@outlook.com>
2025-05-29 02:11:47 +0100

    修改ui，增加button（1阶模型转换成2阶）

--- src/ui/events.rs
+++ src/ui/events.rs
@@ -9,3 +9,14 @@ pub struct LoadModelEvent(pub PathBuf);
 
 #[derive(Event)]
 pub struct ToggleWireframeEvent;
+
+#[derive(Event)]
+pub struct ConvertToHigherOrderEvent {
+    pub order: u32, // mesh order, 2 for second order, 3 for third order, etc.
+}
+
+impl Default for ConvertToHigherOrderEvent {
+    fn default() -> Self {
+        Self { order: 2 } // default to second order mesh
+    }
+}
